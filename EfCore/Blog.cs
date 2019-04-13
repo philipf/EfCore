182fe501814683659c17873a044a7e41aa44eb07
philipf <philipf@agilebridge.co.za>
2019-04-13 10:01:26 +0200

    Additional comments added

--- EfCore/Blog.cs
+++ EfCore/Blog.cs
@@ -7,9 +7,8 @@ namespace EfCore
         public int BlogId { get; set; }
         public string Url { get; set; }
 
-        private readonly ICollection<Post> _posts;
-
-        public IEnumerable<Post> Posts => _posts;
+        private readonly ICollection<Post> _posts;  // Define a private backing field
+        public IEnumerable<Post> Posts => _posts;   // Only expose a getter
 
         public Blog()
         {
