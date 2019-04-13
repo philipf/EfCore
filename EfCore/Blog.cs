aa48a4f3edfe079c35c7bf3408f20e5cb8120053
Philip Fourie <philipf@users.noreply.github.com>
2019-04-13 15:22:38 +0200

    Removed readonly keyword
    The backing field must be writable by EF core

--- EfCore/Blog.cs
+++ EfCore/Blog.cs
@@ -7,7 +7,7 @@ namespace EfCore
         public int BlogId { get; set; }
         public string Url { get; set; }
 
-        private readonly ICollection<Post> _posts;  // Define a private backing field
+        private ICollection<Post> _posts;  // Define a private backing field
         public IEnumerable<Post> Posts => _posts;   // Only expose a getter
 
         public Blog()
