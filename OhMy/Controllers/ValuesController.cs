6e802a6bbad5c1dc042b987b6878f8fe4218ca9f
philipf <philipf@agilebridge.co.za>
2019-12-31 16:09:22 +0200

    Made responses OData compliant

--- OhMy/Controllers/ValuesController.cs
+++ OhMy/Controllers/ValuesController.cs
@@ -7,43 +7,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace OhMy.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+    [Produces("application/json")]
+    public class BlogController : ODataController
     {
         // GET api/values
-        [HttpGet]
         [EnableQuery]
-        public ActionResult<IEnumerable<Blog>> Get()
+        public IQueryable<Blog> Get()
         {
             var ctx = new MyDbContext();
             return ctx.Blogs;
             //return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
--- OhMy/Startup.cs
+++ OhMy/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OData.Edm;
+using Microsoft.OData;
+using Microsoft.AspNet.OData.Formatter;
 
 namespace OhMy
 {
@@ -25,13 +29,21 @@ namespace OhMy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddOData();
+            services.AddTransient<BlogModelBuilder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, BlogModelBuilder modelBuilder)
         {
+            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+
+            builder.EntitySet<Blog>("Blogs");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,11 +53,43 @@ namespace OhMy
 
             app.UseMvc(routeBuilder => {
 
+                //routeBuilder.MapODataServiceRoute("odata", "odata", GetEdmModel());
+                routeBuilder.MapODataServiceRoute("ODataRoutes", "odata", modelBuilder.GetEdmModel(app.ApplicationServices));
                 routeBuilder.EnableDependencyInjection();
 
                 routeBuilder.Expand().Select().OrderBy().Filter();
 
+
             });
+        }
+
+        public class BlogModelBuilder
+        {
+            public IEdmModel GetEdmModel(IServiceProvider serviceProvider)
+            {
+                var builder = new ODataConventionModelBuilder(serviceProvider);
+                builder.EntitySet<Blog>(nameof(Blog))
+                    .EntityType
+                    .Filter() // Allow for the $filter Command
+                    .Count() // Allow for the $count Command
+                    .Expand() // Allow for the $expand Command
+                    .OrderBy() // Allow for the $orderby Command
+                    .Page() // Allow for the $top and $skip Commands
+                    .Select() // Allow for the $select Command; 
+                    .ContainsMany(x => x.Posts);
+
+                builder.EntitySet<Post>(nameof(Post))
+                    .EntityType
+                    .Filter() // Allow for the $filter Command
+                    .Count() // Allow for the $count Command
+                    .Expand() // Allow for the $expand Command
+                    .OrderBy() // Allow for the $orderby Command
+                    .Page() // Allow for the $top and $skip Commands
+                    .Select() // Allow for the $select Command
+                    .Expand(); 
+              
+                return builder.GetEdmModel();
+            }
         }
     }
 }
